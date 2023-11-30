9b5fdc72e22993037b33f3d7f5d1a6f3d277649c
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2023-11-30 10:27:30 -0800

    sending image version 1
    image sent as a code, cannot vistualize still

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import base64 # for sending images 
+import time
 
 class Peer:
     def __init__(self, host, port):
@@ -23,18 +25,20 @@ class Peer:
             self.connections.append(connection)
             print(f"Accepted connection from {address}")
             threading.Thread(target=self.handle_client, args=(connection, address)).start()
+    def broadcast(self,data):
+        self.send_date(data)
 
-    def send_data(self, data, target=None):
+    def send_data(self, data, target=None, is_image=False):
         for connection in self.connections:
             try:
                 if target is None or connection == target:
-                    connection.sendall(data.encode())
+                    if is_image:
+                        connection.sendall(base64.b64encode(data))
+                    else:
+                        connection.sendall(data.encode())
             except socket.error as e:
                 print(f"Failed to send data. Error: {e}")
                 self.connections.remove(connection)
-
-    def broadcast(self, data):
-        self.send_data(data)
 
     def handle_client(self, connection, address):
         while True:
@@ -42,14 +46,19 @@ class Peer:
                 data = connection.recv(1024)
                 if not data:
                     break
-                print(f"Received data from {address}: {data.decode()}")
+                if data.startswith(b"image:"):
+                    image_data = base64.b64decode(data[len("image:"):])
+                    # Handle the image_data as needed, e.g., save to a file or process it
+                    print(f"Received image from {address}")
+                else:
+                    print(f"Received data from {address}: {data.decode()}")
             except socket.error:
                 break
 
         print(f"Connection from {address} closed.")
         self.connections.remove(connection)
         connection.close()
-
+    
     def start(self):
         listen_thread = threading.Thread(target=self.listen)
         listen_thread.start()
@@ -66,22 +75,24 @@ if __name__ == "__main__":
     node3.start()
 
     # Give some time for nodes to start listening
-    import time
+    
     time.sleep(2)
 
-    node2.connect("127.0.0.1", 8000)
+    node2.connect("127.0.0.1", 8000) # work
     time.sleep(1)  # Allow connection to establish
 
 
     node2.connect("127.0.0.1",8002)# works
     time.sleep(1)
     
+    
 
 
     while True:
         print("1. Send a message")
-        print("2. Broadcast a message")
-        print("3. Exit")
+        print("2. Send an image")
+        print("3. Broadcast a message")
+        print("4. Exit")
         choice = input("Enter your choice: ")
 
         if choice == '1':
@@ -95,11 +106,20 @@ if __name__ == "__main__":
                 print("Target not found.")
 
         elif choice == '2':
+            image_path = input("Enter the path to the image file: ")
+            try:
+                with open(image_path, "rb") as image_file:
+                    image_data = image_file.read()
+                    node2.send_data(b"image:" + image_data, is_image=True)
+            except FileNotFoundError:
+                print("Image file not found.")
+
+        elif choice == '3':
             message = input("Enter broadcast message: ")
             node2.broadcast(f"Broadcast from node2: {message}")
 
-        elif choice == '3':
+        elif choice == '4':
             break
 
         else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
+            print("Invalid choice. Please enter 1, 2, 3, or 4.")
